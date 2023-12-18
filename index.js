f1ab5a01c0e118c2d04b3f62a8d66ce70a44fdc3
zxg <z975203723@gmail.com>
2023-12-18 17:16:41 +0800

    feat: :sparkles: TypeScript

---
+++ index.js
@@ -0,0 +1,15 @@
+var num = 123;
+var str = 'Hello TypeScript !';
+var numToString = "".concat(num);
+var a = null;
+var b = undefined;
+// 需要关闭严格模式才行
+// let a1:void = null
+// let b1:void = undefined
+console.log(str);
+console.log(num);
+console.log(numToString);
+console.log(a);
+console.log(b);
+// console.log(a1);
+// console.log(b1);
---
+++ index.ts
@@ -0,0 +1,17 @@
+let num:number = 123
+let str:string = 'Hello TypeScript !'
+let numToString:string = `${num}`
+let a:null = null
+let b:undefined = undefined
+
+// 需要关闭严格模式才行
+// let a1:void = null
+// let b1:void = undefined
+
+console.log(str)
+console.log(num)
+console.log(numToString)
+console.log(a);
+console.log(b);
+// console.log(a1);
+// console.log(b1);
